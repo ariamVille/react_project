6e4db2ee7de51c0243b2d2e3655fde54f32c019b
ariamVille <mariasoulevillanueva@gmail.com>
2018-04-07 00:41:22 +0800

    added button click example

--- react-seminar/src/App.js
+++ react-seminar/src/App.js
@@ -3,19 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  constructor(){
+    super();
+    this.state = {
+      count:0
+    };
+
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count+1
+    });
+  }
+
+  render(){
+    return(
+          < div >
+            <button onClick={this.increment} > increment </button> {
+              this.state.count
+            } 
+            </div>
     );
   }
+
 }
 
 export default App;
